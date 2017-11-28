67bb1223dcc5135efc69d6e3baa4477130287213
Lucas Luitjes <lucas@luitjes.it>
2017-11-28 18:28:18 +0100

    Add hash of response body to output

--- lib/js_tracker/scraper.ex
+++ lib/js_tracker/scraper.ex
@@ -7,21 +7,38 @@ defmodule JsTracker.Scraper do
     :ok = ChromeRemoteInterface.PageSession.subscribe(page_pid, "Page.loadEventFired")
     :ok = ChromeRemoteInterface.PageSession.subscribe(page_pid, "Network.responseReceived")
     {:ok, _} = ChromeRemoteInterface.RPC.Page.navigate(page_pid, %{url: url})
+    result = collect_events(page_pid)
     :ok = Chromesmith.checkin :chrome_pool, true
-    collect_events()
+    result
   end
 
-  defp collect_events(results \\ []) do
+  defp collect_events(page_pid, results \\ []) do
     receive do
       {:chrome_remote_interface, "Page.loadEventFired", _response} ->
         results
       {:chrome_remote_interface, "Network.responseReceived", response} ->
-        payload = response["params"]["response"]["url"]
         if response["params"]["type"] == "Script" do
-          collect_events([ payload | results])
+          collect_events(page_pid, [ format_event(page_pid, response) | results])
         else
-          collect_events(results)
+          collect_events(page_pid, results)
         end
     end
+  end
+
+  defp format_event(page_pid, response) do
+    # TODO clean this up
+    url = response["params"]["response"]["url"]
+    id = response["params"]["requestId"]
+    request_headers = response["params"]["response"]["headers"]
+    response_headers = response["params"]["response"]["requestHeaders"]
+    {:ok, body} = ChromeRemoteInterface.RPC.Network.getResponseBody(page_pid, %{requestId: id})
+    body_hash = :crypto.hash(:sha256, body["result"]["body"])
+    |> Base.encode16(case: :lower)
+    %{
+      url: url,
+      request_headers: request_headers,
+      response_headers: response_headers,
+      body_hash: body_hash
+    }
   end
 end
--- test/scraper_test.exs
+++ test/scraper_test.exs
@@ -2,7 +2,8 @@ defmodule JsTrackerTest do
   use ExUnit.Case
 
   test "Scrape a url" do
-    result = JsTracker.Scraper.scrape("http://localhost:4001")
-    assert result == ["http://localhost:4001/js/app.js"]
+    [result | _] = JsTracker.Scraper.scrape("http://localhost:4001")
+    assert result.url == "http://localhost:4001/js/app.js"
+    assert result.body_hash == "01645e09ad03b4f44cff57f2a008b35e031a181c2cbb672c4309d19ff2734ae7"
   end
 end

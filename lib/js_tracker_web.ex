c478fede1103c6261dbe19e57663a42ca250bc14
Lucas Luitjes <lucas@luitjes.it>
2018-01-16 16:10:10 +0100

    Add some bootstrap and a fancy flash message

--- lib/js_tracker_web.ex
+++ lib/js_tracker_web.ex
@@ -32,7 +32,7 @@ defmodule JsTrackerWeb do
                         namespace: JsTrackerWeb
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
+      import Phoenix.Controller, only: [get_flash: 1, view_module: 1]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
